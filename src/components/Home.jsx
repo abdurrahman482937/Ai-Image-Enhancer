af1674a04417f63664da0071582b25ed2fa1529b
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-03 11:27:31 +0600

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Upload from "./Upload";
+import Preview from "./Preview";
+
+const Home = () => {
+  return (
+    <div>
+      <Upload></Upload>
+      <Preview></Preview>
+    </div>
+  );
+};
+
+export default Home;
