af8c73ea62a4357daa62a35a384c94064569e073
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-04 21:16:07 +0600

    upload file design..

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Upload = () => {
-    return (
-        <div>
-            Upload
-        </div>
-    );
+  return (
+    <div className="bg-white shadow-lg rounded-2xl p-5 w-full max-w-2xl">
+      <label
+        htmlFor="fileInput"
+        className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-6 text-center hover:border-blue-500 transition duration-200"
+      >
+        <input type="file" id="fileInput" className="hidden" />
+        <p>click & drag to upload your image</p>
+      </label>
+    </div>
+  );
 };
 
 export default Upload;
