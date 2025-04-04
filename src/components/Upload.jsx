128ab987f3cc6ec61bae0018a0e58fd44e9bacc8
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-04 23:07:07 +0600

    props..

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 
 const Upload = () => {
+  const showImageFunc = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      props.uploadImageHandler(file);
+    }
+  };
   return (
     <div className="bg-white shadow-lg rounded-2xl p-5 w-full max-w-2xl">
       <label
         htmlFor="fileInput"
         className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-6 text-center hover:border-blue-500 transition-all duration-300"
       >
-        <input type="file" id="fileInput" className="hidden" />
-        <span className="text-lg font-medium text-gray-600">click & drag to upload your image</span>
+        <input
+          type="file"
+          id="fileInput"
+          className="hidden"
+          onChange={showImageFunc}
+        />
+        <span className="text-lg font-medium text-gray-600">
+          click & drag to upload your image
+        </span>
       </label>
     </div>
   );
