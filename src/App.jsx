7a5889d53f727889e83aac278adea5a724f96a30
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-04 21:07:08 +0600

    app..

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div className="">
-      header
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4"
+      style={{ fontFamily: "'Roboto', sans-serif" }}
+    >
+      <div className="text-center mb-8">
+        <h1 className="text-5xl font-bold text-gray-800">AI image enhancer.</h1>
+        <h1 className="text-lg font-semibold font-mono tracking-tighter text-gray-600">
+          Upload your image and let AI enhance to in seconds.
+        </h1>
+      </div>
       <Home></Home>
-      footer
+      <div>
+        <p className="text-sm text-gray-600 mt-6">
+          Powered by @abdurrahman482937
+        </p>
+      </div>
     </div>
   );
 };
