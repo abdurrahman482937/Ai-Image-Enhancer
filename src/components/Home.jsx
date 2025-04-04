4f52337cc821d7bf2962d1539a6fcf591aa5c6d2
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-04 22:14:24 +0600

    useState..

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import { useState } from "react";
 import Upload from "./Upload";
 import Preview from "./Preview";
 
 const Home = () => {
+  const [upload, setUpload] = useState(null);
+  const [enhanced, setEnhanced] = useState(null);
+  const [loading, setLoading] = useState(false);
   return (
     <>
-      <Upload></Upload>
-      <Preview></Preview>
+      <Upload ></Upload>
+      <Preview loading={loading} upload={upload} enhanced={enhanced}></Preview>
     </>
   );
 };
